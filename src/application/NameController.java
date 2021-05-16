bd670bf464c07c2250b69903285c25bbe41b4a88
rehroman <s31464@beuth-hochschule.de>
2021-05-16 21:35:33 +0200

    every scene has its own ControllerClass

--- src/application/NameController.java
+++ src/application/NameController.java
@@ -9,7 +9,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class NameController { // to be moved to MenuController?
+public class NameController extends MenuController {
 	
 	private Parent root;
 	private Stage stage;
@@ -23,14 +23,4 @@ public class NameController { // to be moved to MenuController?
 		stage.setScene(scene);
 		stage.show();
 	}
-	
-	public void switchToMenu (ActionEvent e) throws IOException {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("1_Menu.fxml"));
-		root = loader.load();
-		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
-	}
-
 }
