4cd34a3b79e418e4b3f8838ff8116e9c1b150bfa
rehroman <s31464@beuth-hochschule.de>
2021-06-18 14:41:04 +0200

    updated Game scene and css

--- src/application/GameController.java
+++ src/application/GameController.java
@@ -1,5 +1,38 @@
 package application;
 
-public class GameController {
+
+
+import java.awt.event.ActionListener;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class GameController implements ActionListener {
+	@FXML
+	Label nameLabel;
+	@FXML
+	Label pointsLabel;
+
+	
+	public void displayName(String username) {
+		nameLabel.setText("Spieler: " + username);
+	}
+	
+	public void setPoints(int points) {
+		nameLabel.setText("Punkte: " + points);
+	}
+	
+	@FXML
+	public void exitGame (ActionEvent e) {
+		System.exit(0); //TODO something else?
+	}
+
+	@Override
+	public void actionPerformed(java.awt.event.ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	
 }
--- src/application/NameController.java
+++ src/application/NameController.java
@@ -3,10 +3,12 @@ package application;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class NameController extends MenuController {
@@ -15,11 +17,23 @@ public class NameController extends MenuController {
 	private Stage stage;
 	private Scene scene;
 	
+	@FXML
+	private TextField name_NameField;
+	
 	public void switchToGame (ActionEvent e) throws IOException {
+		String username = name_NameField.getText();
+		
+		
+		
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("3_Game.fxml"));
 		root = loader.load();
+		
+		GameController gameController = loader.getController();
+		gameController.displayName(username);
+		
 		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
 		scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		stage.setScene(scene);
 		stage.show();
 	}
