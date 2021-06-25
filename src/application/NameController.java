18c4d6311c5a832ba3031d2442d7a19d8f415c53
rehroman <s31464@beuth-hochschule.de>
2021-06-25 11:58:18 +0200

    Back Buttons working again

--- src/application/NameController.java
+++ src/application/NameController.java
@@ -46,8 +46,16 @@ public class NameController extends MenuController {
 		gameController.init(root);
 	}
 
-	public void switchToMenu (ActionEvent e) throws IOException {
-		/*TODO: Inhalt*/
+
+		public void switchToMenu (ActionEvent e) throws IOException {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("1_Menu.fxml"));
+			root = loader.load();
+			stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setScene(scene);
+			stage.show();
+		
 	}
 
 
